cfd2f5134e3660254bce2ffa6c50c3fc55e9795a
java0088 <965002107@qq.com>
2021-06-14 13:44:49 +0800

    连接数据库

--- config/config.default.js
+++ config/config.default.js
@@ -22,31 +22,31 @@ module.exports = appInfo => {
   const userConfig = {
     // myAppName: 'egg',
   };
-  // // 配置mysql
-  // config.sequelize = {
-  //   dialect: 'mysql',
-  //   host: BASE_URL,
-  //   port: 3306,
-  //   database: 'hwthub',
-  //   username: 'root',
-  //   password: '123456',
-  //   define: { // model的全局配置
-  //     timestamps: true, // 添加create,update,delete时间戳
-  //     // paranoid: true, // 添加软删除
-  //     freezeTableName: true, // 防止修改表名为复数
-  //     underscored: false // 防止驼峰式字段被默认转为下划线
-  //   },
-  //   timezone: '+08:00', // 由于orm用的UTC时间，这里必须加上东八区，否则取出来的时间相差8小时
-  //   dialectOptions: { // 让读取date类型数据时返回字符串而不是UTC时间
-  //     dateStrings: true,
-  //     typeCast(field, next) {
-  //       if (field.type === 'DATETIME') {
-  //         return field.string()
-  //       }
-  //       return next()
-  //     }
-  //   }
-  // }
+  // 配置mysql
+  config.sequelize = {
+    dialect: 'mysql',
+    host: BASE_URL,
+    port: 3306,
+    database: 'hwthub',
+    username: 'root',
+    password: '123456',
+    define: { // model的全局配置
+      timestamps: true, // 添加create,update,delete时间戳
+      // paranoid: true, // 添加软删除
+      freezeTableName: true, // 防止修改表名为复数
+      underscored: false // 防止驼峰式字段被默认转为下划线
+    },
+    timezone: '+08:00', // 由于orm用的UTC时间，这里必须加上东八区，否则取出来的时间相差8小时
+    dialectOptions: { // 让读取date类型数据时返回字符串而不是UTC时间
+      dateStrings: true,
+      typeCast(field, next) {
+        if (field.type === 'DATETIME') {
+          return field.string()
+        }
+        return next()
+      }
+    }
+  }
 
   // 配置redis
   config.redis = {
